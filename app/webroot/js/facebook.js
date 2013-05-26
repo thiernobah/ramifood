7487f76879363807cf6ece86f871dd36816f2934
unknown <thierno.bah@live.fr>
2013-05-27 01:17:18 +0200

    fbc

--- app/webroot/js/facebook.js
+++ app/webroot/js/facebook.js
@@ -5,7 +5,7 @@ window.fbAsyncInit = function() {
         status: true, // check login status
         cookie: true, // enable cookies to allow the server to access the session
         xfbml: true,  // parse XFBML
-        Oauth:true
+       // Oauth:true
     });
     FB.api('/me', function(user) {
         if (user) {
@@ -40,8 +40,8 @@ $(function() {
             } else {
                 console.log('User cancelled login or did not fully authorize.');
             }
-        },{scope:'email, username, first_name, last_name, birthday, picture, facebook_id'});
+        },{scopes:'email, username, first_name, last_name, birthday, picture, facebook_id'});
         
         return false;
     });
-})
+});
